3fea6ae59c0e199fbe7d5cee054b67810fbb77ef
Yassine Hl <hlioui.yassine@gmail.com>
2013-12-16 18:12:21 +0100

    update project : add SDL library support

---
+++ Premake/maConfig.lua
@@ -0,0 +1,22 @@
+solution "solTest"
+	configurations { "debug", "release" }
+	language "C++"
+	includedirs { "../src", "../libs/includes" }
+	location "solution"
+	libdirs { "../libs" }
+
+configuration "debug"
+	defines { "MY_DEBUG" }
+	flags { "Symbols" }
+	targetdir "../bin/debug"
+	targetsuffix "_d"
+
+configuration "release"
+	targetdir "../bin/release"
+	targetsuffix "_r"
+
+
+	project "projTest"
+		kind "ConsoleApp"
+		files { "../src/*.h", "../src/*.cpp" }
+         	links { "SDL2" , "SDL2main" }
