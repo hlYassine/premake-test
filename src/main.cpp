ef8793139699618acc1a4f2cb69e168c12f4b28b
yassine hlioui <hlioui.yassine@gmail.com>
2013-12-17 18:52:33 +0000

    SDL update

--- src/main.cpp
+++ src/main.cpp
@@ -3,18 +3,6 @@
 
 int main(int argc, char** argv)
 {
-    if (SDL_Init(SDL_INIT_VIDEO) != 0 )
-    {
-        std::cout<<"Échec de l'initialisation de la SDL "<<SDL_GetError()<<std::endl;  
-    }
-
-    /* Création de la fenêtre */
-    SDL_Window* _Window = NULL;
-    _Window = SDL_CreateWindow("Premake test",SDL_WINDOWPOS_UNDEFINED,
-                                                  SDL_WINDOWPOS_UNDEFINED,
-                                                  640,
-                                                  480,
-                                                  SDL_WINDOW_SHOWN);
 
 #ifdef MY_DEBUG
    int numdrivers = SDL_GetNumVideoDrivers();
@@ -33,29 +21,60 @@ int main(int argc, char** argv)
          printf("Driver %s doesn't work.\n", drivername);
       }
     }
-
      printf("\n%d video drivers total (%d work)\n", numdrivers, working);
 #endif
 
-    //main loop
-    SDL_Event _event;
-    bool _exit = false;
-    while(!_exit)
-    {
+  if (SDL_Init(SDL_INIT_EVERYTHING) != 0){
+	std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
+	return 1;
+}
 
+  SDL_Window *win = SDL_CreateWindow("Hello World!", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+if (win == 0){
+	std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+	return 1;
+}
+
+SDL_Renderer *ren = SDL_CreateRenderer(win, -1, 
+	SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+if (ren == 0){
+	std::cout << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
+	return 1;
+}
+
+SDL_Surface *bmp = SDL_LoadBMP("../../resources/hello.bmp");
+if (bmp == 0){
+	std::cout << "SDL_LoadBMP Error: " << SDL_GetError() << std::endl;
+	return 1;
+}
+
+SDL_Texture *tex = SDL_CreateTextureFromSurface(ren, bmp);
+SDL_FreeSurface(bmp);
+if (tex == 0){
+	std::cout << "SDL_CreateTextureFromSurface Error: " << SDL_GetError() << std::endl;
+	return 1;
+}
+
+SDL_Event _event;
+bool _exit = false;
+
+while(!_exit)
+{
 	SDL_WaitEvent(&_event);
 
-        if(_event.window.event == SDL_WINDOWEVENT_CLOSE)
-		_exit = true;
-    }
+	if(_event.window.event == SDL_WINDOWEVENT_CLOSE)
+           _exit = true;
 
+	SDL_RenderClear(ren);
+	SDL_RenderCopy(ren, tex, NULL, NULL);
+	SDL_RenderPresent(ren);
+}
 
-    if( _Window )
-    {
-       SDL_DestroyWindow(_Window);
-    }
+SDL_DestroyTexture(tex);
+SDL_DestroyRenderer(ren);
+SDL_DestroyWindow(win);
+SDL_Quit();
 
-    SDL_Quit();
 
 
 	return 0;
